2f5eef6e9c25322f77c73d6fa64f34c5200c4f88
Gabriel Araujo <gabrielaraujof@outlook.com.br>
2017-11-02 00:23:25 -0300

    Restart game added
    - added an event for checking the game score on every attack
    - added the option to restart de game
    - added a victory message

--- src/Game.vue
+++ src/Game.vue
@@ -10,6 +10,7 @@
         :life="player2.life"
         @attacking="attacks(player1, $event.attackDamage)">
     </player>
+    <button @click="restart">Reiniciar</button>
   </div>
 </template>
 
@@ -32,18 +33,46 @@ export default {
         name: "Jogador 2",
         life: 100,
       },
+      gameRunning: true,
     }
   },
 
   methods: {
+    $_game_showVictoryMessage() {
+      if (this.player1.life > this.player2.life) {
+        alert(`Vitória! ${this.player1.name} venceu ${this.player2.name}`);
+      } else {
+        alert(`Vitória! ${this.player2.name} venceu ${this.player1.name}`);
+      }
+    },
+    $_game_checkGame() {
+      if (this.player1.life && this.player2.life) {
+        return;
+      } else {
+        this.gameRunning = false;
+        this.$_game_showVictoryMessage();
+      }
+    },
     attacks(player, damage) {
       if (player.life > damage) {
         player.life -= damage;
       } else {
         player.life = 0;
       }
+
+      this.$emit('attacked');
+    },
+    restart() {
+      this.player1.life = 100;
+      this.player2.life = 100;
     },
   },
+
+  created() {
+    this.$on('attacked', () => {
+      this.$_game_checkGame();
+    })
+  }
 }
 </script>
 
