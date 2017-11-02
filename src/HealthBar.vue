0ed3ee2bd416514615179c91816bce7dc0af29c0
Gabriel Araujo <gabrielaraujof@outlook.com.br>
2017-11-02 12:09:56 -0300

    Styling the damage bar

--- src/HealthBar.vue
+++ src/HealthBar.vue
@@ -3,7 +3,10 @@
     <div
         class="health"
         :style="{width: `${health}%`}">
-      <div class="damage"></div>
+    </div>
+    <div
+        class="damage"
+        :style="{width: `${health}%`}">
     </div>
   </div>
 </template>
@@ -18,18 +21,28 @@ export default {
 
 <style lang="scss" scoped>
 .health-bar {
+  position: relative;
   border-radius: 2vw;
   border: 1px solid #aaa;
   background-color: #eee;
   height: 2vw;
   overflow: hidden;
   width: 100%;
+  box-shadow: 1px 1px 4px 1px #ddd;
+}
+
+.damage {
+  position: absolute;
+  background-color: #ee4848;
+  box-sizing: border-box;
+  height: 100%;
+  transition: width .5s linear;
 }
 
 .health {
+  @extend .damage;
   background-color: #3c3;
-  box-sizing: border-box;
-  height: 100%;
   transition: width .1s linear;
+  z-index: 999;
 }
 </style>
