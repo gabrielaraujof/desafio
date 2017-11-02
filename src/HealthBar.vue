bfd159f69ba403daa3c593ff120a65c67b52bee4
Gabriel Araujo <gabrielaraujof@outlook.com.br>
2017-11-02 15:08:30 -0300

    Added animation for when being hit

--- src/HealthBar.vue
+++ src/HealthBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="health-bar">
     <div
-        class="health"
+        :class="['health', {'being-hit': beingHit}]"
         :style="{width: `${health}%`}">
     </div>
     <div
@@ -15,34 +15,64 @@
 export default {
   name: 'health-bar',
 
-  props: ['health']
+  props: ['health'],
+
+  data() {
+    return {
+      beingHit: false,
+    };
+  },
+
+  watch: {
+    health(newHealth, oldHealth) {
+      if (newHealth < oldHealth && !this.beingHit) {
+        this.beingHit = true;
+        setTimeout(() => this.beingHit = false, 250);
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .health-bar {
-  position: relative;
+  background-color: #eee;
   border-radius: 2vw;
   border: 1px solid #aaa;
-  background-color: #eee;
+  box-shadow: 1px 1px 4px 1px #ddd;
   height: 2vw;
   overflow: hidden;
+  position: relative;
   width: 100%;
-  box-shadow: 1px 1px 4px 1px #ddd;
 }
 
 .damage {
-  position: absolute;
   background-color: #ee4848;
   box-sizing: border-box;
   height: 100%;
+  position: absolute;
   transition: width .5s linear;
 }
 
 .health {
   @extend .damage;
+
   background-color: #3c3;
   transition: width .1s linear;
   z-index: 999;
+
+  &.being-hit {
+    animation-name: damaging;
+    animation-duration: .1s;
+    animation-iteration-count: 4;
+    animation-direction: alternate;
+    animation-timing-function: linear;
+  }
+}
+
+@keyframes damaging {
+	to {
+		background-color: #e50000;
+	}
 }
 </style>
