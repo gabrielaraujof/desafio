5d4a61fe085668e1e1ce541ed983fea7fb460824
Gabriel Araujo <gabrielaraujof@outlook.com.br>
2017-11-02 00:01:58 -0300

    Player cmp added
    - Player component created
    - added two players in the game!

---
+++ src/Game.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <player
+        :name="player1.name"
+        :life="player1.life">
+    </player>
+    <player
+        :name="player2.name"
+        :life="player2.life">
+    </player>
+  </div>
+</template>
+
+<script>
+import Player from './Player.vue';
+export default {
+  name: 'game',
+
+  components: {
+    Player
+  },
+
+  data () {
+    return {
+      player1: {
+        name: "Jogador 1",
+        life: 100,
+      },
+      player2: {
+        name: "Jogador 2",
+        life: 100,
+      },
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#app {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 60px;
+  margin: auto;
+  text-align: center;
+  width: 980px;
+
+  .player {
+    width: 50%;
+
+    @media (max-width: 568px) {
+      width: 100%;
+    }
+  }
+}
+</style>
---
+++ src/Player.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="player">
+      <h1>{{name}}</h1>
+      <h3>{{life}}%</h3>
+      <button>Atacar</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'player',
+
+  props: ['name', 'life'],
+
+  data() {
+    return {
+      attackDamage: 20,
+    }
+  }
+}
+</script>
